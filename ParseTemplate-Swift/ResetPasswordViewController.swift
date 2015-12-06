43062cf906293e40eadcb20c0994cc5e2f1fcec4
Norbert Metz <nmetz@Norberts-MacBook-Air.local>
2015-12-06 23:11:42 +0100

    Added Password Reset view and navigation to and from

---
+++ ParseTemplate-Swift/ResetPasswordViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ResetPasswordViewController.swift
+//  ParseTemplate-Swift
+//
+//  Created by Norbert Metz on 06/12/2015.
+//  Copyright © 2015 Norbert Metz. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Parse
+import UIKit
+
+class ResetPasswordViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        emailTextField.delegate     = self
+
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if textField == emailTextField {
+            emailTextField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
+    // MARK: Button Actions
+    
+    @IBAction func cancelButtonTapped(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func resetPasswordButtonTapped(sender: UIButton) {
+        print("Reset Password Button Tapped")
+        
+        //TODO: Validate email
+        
+        //TODO: Reset password
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
