3ff393e72b5942b360ca735726e6e0eb9e3d79d1
Norbert Metz <nmetz@norberts-air>
2015-12-06 10:35:12 +0000

    Added bridging header

---
+++ ParseTemplate-Swift/ParseTemplate-Swift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ParseTemplate-Swift-Bridging-Header.h
+//  ParseTemplate-Swift
+//
+//  Created by Norbert Metz on 06/12/2015.
+//  Copyright © 2015 Norbert Metz. All rights reserved.
+//
+
+#ifndef ParseTemplate_Swift_Bridging_Header_h
+#define ParseTemplate_Swift_Bridging_Header_h
+
+#import "MBProgressHUD.h"
+
+#endif
