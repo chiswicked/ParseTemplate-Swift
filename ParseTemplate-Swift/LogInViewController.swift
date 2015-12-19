1330f82a2bb2bf42d4298f6c6e1629d984dc53d8
chiswicked <mr.norbert.metz@googlemail.com>
2015-12-19 17:44:05 +0000

    Removed unnecessary prepareForSegue

--- ParseTemplate-Swift/LogInViewController.swift
+++ ParseTemplate-Swift/LogInViewController.swift
@@ -195,12 +195,6 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         performSegueWithIdentifier("fromLogInToSignUp", sender: nil)
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "fromLogInToSignUp" {
-            print("Segueing fromLogInToSignUp")
-        }
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
